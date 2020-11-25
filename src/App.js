8330a81e08806962697005a8b3216142f4075a79
Julio Bermudez <julenbero@gmail.com>
2020-11-24 21:09:51 -0500

    Exercise Complete

--- src/App.js
+++ src/App.js
@@ -3,39 +3,40 @@ import React, { Component } from 'react';
 /// Mostrar una lista en base a un arreglo
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: ['Javascript', 'CSS', 'HTLM', 'React']
-    }
-  }
-  render() {
-    return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>Las tecnologias del Front</h1>
-        <ul style={styles.list}>
-          {/* Tu codigo va aca */}
-        </ul>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			list : [ 'Javascript', 'CSS', 'HTLM', 'React' ]
+		};
+	}
+	render() {
+		return (
+			<div style={styles.container}>
+				<h1 style={styles.title}>Las tecnologias del Front</h1>
+				<ul style={styles.list}>
+					{/* Tu codigo va aca */}
+					{this.state.list.map((item, i) => <li key={i}>{item}</li>)}
+				</ul>
+			</div>
+		);
+	}
 }
 
 const styles = {
-  container: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  title: {
-    marginTop: '100px'
-  },
-  list: {
-    fontSize: '20px',
-    flexGrow: 6
-  }
-}
+	container : {
+		height         : '100vh',
+		display        : 'flex',
+		flexDirection  : 'column',
+		justifyContent : 'center',
+		alignItems     : 'center'
+	},
+	title     : {
+		marginTop : '100px'
+	},
+	list      : {
+		fontSize : '20px',
+		flexGrow : 6
+	}
+};
 
 export default App;
